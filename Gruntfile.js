1c7cea4544a0c30cafa7aeee65a5da9e56e76250
Stuart Olivera <stuart@stuartolivera.com>
2014-12-12 16:33:14 -0500

    Added base structure & imported demo from ianlunn

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          base: './public',
+          port: '4000',
+          host: '*'
+        }
+      }
+    },
+    clean: {
+      build: {
+        src: ['public/javascripts', 'public/stylesheets']
+      }
+    },
+    coffee: {
+      compile: {
+        options: {
+          bare: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'app/coffeescript',
+          src: ['**/*.coffee', '**/*.js'],
+          dest: 'public/javascripts',
+          ext: '.js'
+        }]
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          bundleExec: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'app/stylesheets',
+          src: ['**/*.sass'],
+          dest: 'public/stylesheets',
+          ext: '.css'
+        }]
+      }
+    },
+    haml: {
+      dist: {
+        options: {
+          bundleExec: true,
+        },
+        files: [{
+          expand: true,
+          cwd: 'app/views',
+          src: ['**/*.haml'],
+          dest: 'public',
+          ext: '.html'
+        }]
+      }
+    },
+    watch: {
+      coffee: {
+        files: ['app/coffeescript/**/*.coffee'],
+        tasks: ['coffee']
+      },
+      styles: {
+        files: ['app/stylesheets/**/*.sass'],
+        tasks: ['sass']
+      },
+      haml: {
+        files: ['app/views/**/*.haml'],
+        tasks: ['haml']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-haml2html');
+
+  grunt.registerTask('build', ['clean', 'coffee', 'sass', 'haml']);
+  grunt.registerTask('default', ['build', 'connect', 'watch']);
+};
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ audiophile
 ==========
 
 Website for information about audiophiles and high quality audio
+
+Based on [Ian Lunn](http://www.ianlunn.co.uk/)'s [jQuery Parralax](https://github.com/IanLunn/jQuery-Parallax).
