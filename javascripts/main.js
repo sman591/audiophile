73b9c475736cfbe7170d8943ed1acd04384ae189
Stuart Olivera <stuart@stuartolivera.com>
2014-12-12 18:21:13 -0500

    Updates

---
+++ javascripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#nav").localScroll(800);
+  $(".continue").localScroll(800);
+  $("#intro").parallax("50%", 0.1);
+  $("#factors").parallax("50%", 0.1);
+  $(".bg").parallax("50%", 0.4);
+  $("#third").parallax("50%", 0.3);
+});
